a27885475d7c0737bdd428e3db10015f2f733239
simonen <si.petrov@yahoo.com>
2022-06-04 21:30:09 +0300

    Initial commit

--- Data Types and Variables/Data Types and Variables - More Exercises/04. Balanced Brackets.py
+++ Data Types and Variables/Data Types and Variables - More Exercises/04. Balanced Brackets.py
@@ -1,6 +1,5 @@
 n = int(input())
 
-previous_bracket = ''
 balance = 1
 brackets = ["(", ")"]
 pair = ''
@@ -8,7 +7,6 @@ pair = ''
 for i in range(1, n + 1):
     bracket = input()
     if bracket in brackets:
-        previous_bracket = bracket
         pair += bracket
         if pair == "()":
             balance = 0
