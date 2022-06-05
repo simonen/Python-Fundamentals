7a12604d7d6fcc3be6d0031e2900ea4f5dab1db0
simonen <si.petrov@yahoo.com>
2022-06-05 20:40:48 +0300

    Initial commit

--- Data Types and Variables/Data Types and Variables - Exercise/04. Sum of Chars.py
+++ Data Types and Variables/Data Types and Variables - Exercise/04. Sum of Chars.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 sum = 0
-for i in range(1, n + 1):
+for _ in range(1, n + 1):
     char = input()
     sum += ord(char)
 
--- Data Types and Variables/Data Types and Variables - Exercise/09. Snowballs.py
+++ Data Types and Variables/Data Types and Variables - Exercise/09. Snowballs.py
@@ -6,14 +6,14 @@ m_time = 0
 m_quality = 0
 
 for i in range(1, n + 1):
-   weight = int(input())
-   time = int(input())
-   quality = int(input())
-   value = (weight / time ) ** quality
-   if value > max_value:
-       max_value = value
-       m_weight = weight
-       m_time = time
-       m_quality = quality
+    weight = int(input())
+    time = int(input())
+    quality = int(input())
+    value = (weight / time) ** quality
+    if value > max_value:
+        max_value = value
+        m_weight = weight
+        m_time = time
+        m_quality = quality
 
 print(f"{m_weight} : {m_time} = {max_value:.0f} ({m_quality})")
