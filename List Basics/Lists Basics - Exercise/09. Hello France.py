6b1699fef27d0987b963b077b0faccc5a174387f
simonen <si.petrov@yahoo.com>
2022-06-08 21:11:49 +0300

    Initial commit

---
+++ List Basics/Lists Basics - Exercise/09. Hello France.py
@@ -0,0 +1,34 @@
+items = input()
+budget = float(input())
+
+A = items.split("->")
+A = " ".join(A)
+A = A.split("|")
+
+profit = 0
+income = 0
+expense = 0
+
+for item in A:
+    price = float(item.split()[1])
+    purchase = (
+        ("Clothes" in item and 0.0 < price <= 50.0 and budget >= price) or
+        ("Shoes" in item and 0.0 < price <= 35.0 and budget >= price) or
+        ("Accessories" in item and 0.0 < price <= 20.5 and budget >= price)
+    )
+    if purchase:
+        print(f"{(price * 1.4):.2f}", end=" ")
+        income += price * 1.4
+        expense += price
+        budget -= price
+print()
+
+profit = income - expense
+
+print(f"Profit: {profit:.2f}")
+if budget + income >= 150:
+    print("Hello, France!")
+else:
+    print("Not enough money.")
+
+
