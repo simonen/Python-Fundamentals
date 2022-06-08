d73dd5da2a02469292d084f549b5a4c21db08eee
simonen <si.petrov@yahoo.com>
2022-06-09 01:35:45 +0300

    Initial commit

--- List Basics/Lists Basics - Exercise/08. Seize the Fire.py
+++ List Basics/Lists Basics - Exercise/08. Seize the Fire.py
@@ -1,26 +1,23 @@
-fires_cells = input()
+fires_cells = input().split("#")
 water = int(input())
 
 high = range(81, 126)
 mid = range(51, 81)
 low = range(1, 51)
 
-cell_list = fires_cells.split(" = ")
-cell_list = " ".join(cell_list)
-cell_list = cell_list.split("#")
-
 print("Cells:")
 total_fire = 0
 
-for cell in cell_list:
-    fire = int(cell.split()[1])
+for cell in fires_cells:
+    split_cell = cell.split(" = ")
+    fire = int(split_cell[1])
     fire_command = (
             ("High" in cell and fire in high and water >= fire) or
             ("Medium" in cell and fire in mid and water >= fire) or
             ("Low" in cell and fire in low and water >= fire)
     )
     if fire_command:
-        print(f" - {cell.split()[1]}")
+        print(f" - {split_cell[1]}")
         total_fire += fire
         water -= fire
 
