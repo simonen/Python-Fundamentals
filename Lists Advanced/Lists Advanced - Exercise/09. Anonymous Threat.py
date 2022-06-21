3c3dbf2047fdadb23cfa870eba4ca61c1e245fdd
simonen <si.petrov@yahoo.com>
2022-06-21 21:29:28 +0300

    initial commit

--- Lists Advanced/Lists Advanced - Exercise/09. Anonymous Threat.py
+++ Lists Advanced/Lists Advanced - Exercise/09. Anonymous Threat.py
@@ -1,39 +1,35 @@
-A = input()
-A = A.split(" ")
+A = list(input().split(" "))
+
 
 command = input()
 
 while command != "3:1":
-    # COMMAND BREAKDOWN
     command = command.split(" ")
     action = command[0]
     start_in = int(command[1])
     end_in = int(command[2])
     if start_in < 0:
         start_in = 0
-    ###### DIVIDE
+
     if action == "divide" and start_in <= len(A):
-        B = A.pop(start_in)
-        step = int(len(B) / end_in)
-        pos = start_in
+        string = A.pop(start_in)
+        step = int(len(string) / end_in)
+        B = []
+        index = start_in
         for i in range(1, end_in):
-            substring = B[0:step]
-            B = B[step::]
-            A.insert(pos, substring)
-            pos += 1
-        if len(B) >= step:
-            A.insert(pos, B)
-        #print(A)
-    ######## MERGE
+            chars = string[0:step]
+            A.insert(index, chars)
+            index += 1
+            string = string[step::]
+        A.insert(index, string)
+
     elif action == "merge":
         if end_in >= len(A):
             end_in = len(A) - 1
         if start_in < end_in:
-            start = start_in
             for j in A[start_in:end_in]:
                 A[start_in] += A.pop(start_in + 1)
 
-    #print(A)
     command = input()
 
 A = " ".join(A)
