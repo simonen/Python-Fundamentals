1d71cd02dca1b933d40b61aaf7cdb158d0dd4fdf
simonen <si.petrov@yahoo.com>
2022-06-22 00:34:43 +0300

    initial commit

---
+++ Mid Exams/01. Programming Fundamentals Mid Exam Retake/02. The Lift.py
@@ -0,0 +1,21 @@
+people = int(input())
+curr_lift = list(map(int, input().split(" ")))
+
+for i, cabin in enumerate(curr_lift):
+    if curr_lift[i] < 4:
+        diff = 4 - curr_lift[i]
+        if people >= diff:
+            people -= diff
+            curr_lift[i] += diff
+        else:
+            curr_lift[i] += people
+            people = 0
+            break
+
+if min(curr_lift) < 4:
+    print("The lift has empty spots!")
+
+if people > 0:
+    print(f"There isn't enough space! {people} people in a queue!")
+
+print(" ".join(map(str, curr_lift)))
