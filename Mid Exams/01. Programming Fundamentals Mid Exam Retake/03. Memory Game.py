0b1b41b29444d5715e6bc8d754643f78db8f64ce
simonen <si.petrov@yahoo.com>
2022-06-23 20:40:36 +0300

    initial commit

--- Mid Exams/01. Programming Fundamentals Mid Exam Retake/03. Memory Game.py
+++ Mid Exams/01. Programming Fundamentals Mid Exam Retake/03. Memory Game.py
@@ -7,21 +7,18 @@ while command != "end":
     command_in = command.split()
     index1 = int(command_in[0])
     index2 = int(command_in[1])
+    indices = [index1, index2]
 
     if (0 <= index1 <= len(numbers) - 1) and (0 <= index2 <= len(numbers) - 1) and index1 != index2:
         if numbers[index1] == numbers[index2]:
             print(f"Congrats! You have found matching elements - {numbers[index1]}!")
-            numbers.pop(index1)
-            if index2 > 0:
-                numbers.pop(index2 - 1)
-            else:
-                numbers.pop(index2)
+            numbers = [numbers[x] for x in range(len(numbers)) if x not in indices]
         else:
             print("Try again!")
     else:
         print(f"Invalid input! Adding additional elements to the board")
-        numbers.insert((len(numbers) // 2), "-" + str(turns) + "a")
-        numbers.insert((len(numbers) // 2), "-" + str(turns) + "a")
+        numbers.insert((len(numbers) // 2), f"-{str(turns)}a")
+        numbers.insert((len(numbers) // 2), f"-{str(turns)}a")
 
     if len(numbers) == 0:
         print(f"You have won in {turns} turns!")
