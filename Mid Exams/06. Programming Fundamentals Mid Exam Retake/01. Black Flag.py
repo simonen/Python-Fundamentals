8bcc58c76abce11ef72a57bf4543697d17e361b1
simonen <si.petrov@yahoo.com>
2022-06-23 23:40:09 +0300

    initial commit

---
+++ Mid Exams/06. Programming Fundamentals Mid Exam Retake/01. Black Flag.py
@@ -0,0 +1,21 @@
+days = int(input())
+daily_plunder = int(input())
+expected_plunder = float(input())
+
+total_plunder = 0
+for day in range(1, days + 1):
+    total_plunder += daily_plunder
+    if day % 3 == 0:
+        total_plunder += daily_plunder * 0.5
+    if day % 5 == 0:
+        total_plunder *= 0.7
+
+if expected_plunder > 0:
+    plunder_percent = total_plunder / expected_plunder * 100
+else:
+    plunder_percent = 0
+
+if total_plunder >= expected_plunder:
+    print(f"Ahoy! {total_plunder:.2f} plunder gained.")
+else:
+    print(f"Collected only {plunder_percent:.2f}% of the plunder.")
