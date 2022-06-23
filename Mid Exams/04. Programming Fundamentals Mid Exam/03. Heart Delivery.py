2fa117bc0e7ea64fbe50be4bb4491b755d5e359d
simonen <si.petrov@yahoo.com>
2022-06-23 20:03:28 +0300

    initial commit

---
+++ Mid Exams/04. Programming Fundamentals Mid Exam/03. Heart Delivery.py
@@ -0,0 +1,31 @@
+houses = list(map(int, input().split("@")))
+command = input()
+jump = 0
+index = 0
+
+while command != "Love!":
+    command = list(command.split())
+    jump = int(command[1])
+    index += jump
+    if index > len(houses) - 1:
+        index = 0
+    if houses[index] == 0:
+        print(f"Place {index} already had Valentine's day.")
+        command = input()
+        continue
+
+    houses[index] -= 2
+    if houses[index] == 0:
+        print(f"Place {index} has Valentine's day.")
+
+    command = input()
+
+
+print(f"Cupid's last position was {index}.")
+failed_houses = len(houses) - houses.count(0)
+
+if sum(houses) == 0:
+    print("Mission was successful.")
+else:
+    print(f"Cupid has failed {failed_houses} places.")
+
