76a42d38b8f05370ded6fa36010c687102550491
simonen <si.petrov@yahoo.com>
2022-06-23 23:23:32 +0300

    initial commit

---
+++ Mid Exams/05. Programming Fundamentals Mid Exam/03. Inventory.py
@@ -0,0 +1,36 @@
+items = list(input().split(", "))
+command = input()
+
+while command != "Craft!":
+    command = command.split(" - ")
+    action = command[0]
+    item = command[1]
+
+    if action == "Collect" and item not in items:
+        items.append(item)
+
+    elif action == "Combine Items":
+        item = command[1].split(":")[0]
+        if item in items:
+            new_item = command[1].split(":")[1]
+            old_item_ind = items.index(item)
+            items.insert(old_item_ind + 1, new_item)
+
+    if item in items:
+        pass
+    else:
+        command = input()
+        continue
+
+    if action == "Drop":
+        items.remove(item)
+
+    elif action == "Renew":
+        item_ind = items.index(item)
+        renewed = items.pop(item_ind)
+        items.append(renewed)
+
+    command = input()
+
+items = ", ".join(items)
+print(items)
