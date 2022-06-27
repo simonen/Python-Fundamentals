9763bdbbda78d07af7efe5ec8e952f7bcc557b50
simonen <si.petrov@yahoo.com>
2022-06-27 23:34:19 +0300

    initial commit

--- Lists Advanced/Lists Advanced - Exercise/07. Group of 10's.py
+++ Lists Advanced/Lists Advanced - Exercise/07. Group of 10's.py
@@ -4,11 +4,8 @@ numbers = list(map(int, input().split(", ")))
 max_group = ceil(max(numbers) / 10) * 10
 
 for group in range(10, max_group + 10, 10):
-    A = []
-    for number in numbers:
-        if group - 10 < number <= group:
-            A.append(number)
-
-    print(f"Group of {group}'s: {A}")
+    filtered = [num for num in numbers if num <= group]
+    numbers = [num for num in numbers if num > group]
+    print(f"Group of {group}'s: {filtered}")
 
 
