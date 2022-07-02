93431559fab1ea1ceef5bc2a3af5c700ceff51e9
simonen <si.petrov@yahoo.com>
2022-07-02 18:56:15 +0300

    initial commit

--- Dictionaries/Dictionaries - Exercise/05. Legendary Farming.py
+++ Dictionaries/Dictionaries - Exercise/05. Legendary Farming.py
@@ -1,39 +1,34 @@
 stash = {"shards": 0, "fragments": 0, "motes": 0}
 key_materials = ["shards", "fragments", "motes"]
 legend_items = ["Shadowmourne", "Valanyr", "Dragonwrath"]
-legend_found = []
+is_legend_found = False
 junk = []
 
 while True:
 
     items = input().lower().split()
     for i in range(0, len(items), 2):
-        key = items[i + 1]
-        value = int(items[i])
-        if key not in stash.keys():
-            stash[key] = value
+        item = items[i + 1]
+        quantity = int(items[i])
+        if item not in stash.keys():
+            stash[item] = quantity
         else:
-            stash[key] += value
+            stash[item] += quantity
 
-        if key in key_materials and stash[key] >= 250:
-            stash[key] = stash[key] - 250
-            l_index = key_materials.index(key)
-            legendary_item = legend_items[l_index]
-            legend_found.append(legendary_item)
+        if item in key_materials and stash[item] >= 250:
+            stash[item] = stash[item] - 250
+            l_index = key_materials.index(item)
             print(f"{legend_items[l_index]} obtained!")
+            is_legend_found = True
             break
-        elif key not in key_materials and key not in junk:
-            junk.append(key)
+        elif item not in key_materials and item not in junk:
+            junk.append(item)
 
-    if len(legend_found) > 0:
+    if is_legend_found:
         break
 
+for k in key_materials:
+    print(f"{k}: {stash[k]}")
 
-for j in key_materials:
-    if j in stash.keys():
-        print(f"{j}: {stash[j]}")
-
-for k in junk:
-    if k in stash.keys():
-        print(f"{k}: {stash[k]}")
-
+for j in junk:
+    print(f"{j}: {stash[j]}")
