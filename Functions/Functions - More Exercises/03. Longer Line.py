c8e978b11931ae8e90b6716c7ba89fa5a37252f1
simonen <si.petrov@yahoo.com>
2022-07-06 22:25:51 +0300

    initial commit

--- Functions/Functions - More Exercises/03. Longer Line.py
+++ Functions/Functions - More Exercises/03. Longer Line.py
@@ -9,18 +9,17 @@ y3 = float(input())
 x4 = float(input())
 y4 = float(input())
 
-a1 = abs(x1 - x2)
-b1 = abs(y1 - y2)
-a2 = abs(x3 - x4)
-b2 = abs(y3 - y4)
 
-if (a1 + b1) > (a2 + b2):
-    if abs(x1) <= abs(x2) or abs(y1) <= abs(y2):
-        print(f"({floor(x1)}, {floor(y1)})({floor(x2)}, {floor(y2)})")
-    else:
-        print(f"({floor(x2)}, {floor(y2)})({floor(x1)}, {floor(y1)})")
+def closest(px1, py1, px2, py2):
+    if abs(px1) <= abs(px2) or abs(py1) <= abs(py2):
+        return f"({floor(px1)}, {floor(py1)})({floor(px2)}, {floor(py2)})"
+    return f"({floor(px2)}, {floor(py2)})({floor(px1)}, {floor(py1)})"
+
+
+line1 = abs(x1 - x2) + abs(y1 - y2)
+line2 = abs(x3 - x4) + abs(y3 - y4)
+
+if line1 > line2:
+    print(closest(x1, y1, x2, y2))
 else:
-    if abs(x3) <= abs(x4) or abs(y3) <= abs(y4):
-        print(f"({floor(x3)}, {floor(y3)})({floor(x4)}, {floor(y4)})")
-    else:
-        print(f"({floor(x4)}, {floor(y4)})({floor(x3)}, {floor(y3)})")
+    print(closest(x3, y3, x4, y4))
