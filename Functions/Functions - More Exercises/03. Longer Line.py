2e155ac87dbc409c6246ecd613ba573a9693a8e9
simonen <si.petrov@yahoo.com>
2022-07-06 22:58:46 +0300

    initial commit

--- Functions/Functions - More Exercises/03. Longer Line.py
+++ Functions/Functions - More Exercises/03. Longer Line.py
@@ -7,14 +7,8 @@ def closest(px1, py1, px2, py2):
     return f"({floor(px2)}, {floor(py2)})({floor(px1)}, {floor(py1)})"
 
 
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
-x3 = float(input())
-y3 = float(input())
-x4 = float(input())
-y4 = float(input())
+x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
+x3, y3, x4, y4 = float(input()), float(input()), float(input()), float(input())
 
 line1 = abs(x1 - x2) + abs(y1 - y2)
 line2 = abs(x3 - x4) + abs(y3 - y4)
