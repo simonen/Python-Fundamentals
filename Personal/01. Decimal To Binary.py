a87d63e0f840485239b44b2ea0af07f22c235eda
simonen <si.petrov@yahoo.com>
2022-07-08 01:33:18 +0300

    initial commit

--- Personal/01. Decimal To Binary.py
+++ Personal/01. Decimal To Binary.py
@@ -1,30 +1,26 @@
-command = input()
-
-
 def binary_num(number_f):
     A = []
     num = int(number_f)
     while num > 0:
-        A.insert(0, str(num % 2))
+        A.append(str(num % 2))
         num = num // 2
+    while len(A) % 4 != 0:
+        A += "0"
 
-    rem = 4 - len(A) % 4
-    if rem != 4:
-        for i in range(rem):
-            A.insert(0, "0")
-
+    A = A[::-1]
     binary = ''
     for p in range(0, len(A), 4):
-        byte_a = "".join(A[:4])
-        binary += byte_a
+        binary += "".join(A[p:p + 4])
         if len(A) > 4:
             binary += " "
-        del A[:4]
 
     return binary
 
 
+command = input()
+
 while command != "stop":
+
     if command == "0":
         print(0)
     else:
