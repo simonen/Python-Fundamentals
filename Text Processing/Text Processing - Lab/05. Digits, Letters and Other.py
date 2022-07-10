b7bce565486ed521c7ffbfc50e6b9bf4137b5e27
simonen <si.petrov@yahoo.com>
2022-07-11 00:17:15 +0300

    initial commit

---
+++ Text Processing/Text Processing - Lab/05. Digits, Letters and Other.py
@@ -0,0 +1,17 @@
+string = input()
+
+digits = ''
+letters = ''
+chars = ''
+
+for s in string:
+    if s.isdigit():
+        digits += s
+    elif s.isalpha():
+        letters += s
+    else:
+        chars += s
+
+print(digits)
+print(letters)
+print(chars)
