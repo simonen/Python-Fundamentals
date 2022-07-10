69fe02f0804abeb0059d0af1d8fee910f54b18c5
simonen <si.petrov@yahoo.com>
2022-07-11 00:06:24 +0300

    initial commit

--- Personal/02. Hexadecimal.py
+++ Personal/02. Hexadecimal.py
@@ -1,18 +1,14 @@
 def hex_dec(numb):
     book = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15}
     decim = 0
-    index = len(num) - 1
-    for n in num:
+    index = len(numb) - 1
+    for n in numb:
         if n in book:
             decim += book[n] * 16 ** index
         else:
             decim += int(n) * 16 ** index
         index -= 1
     return decim
-
-
-# print(hex_dec(num))
-# num = list(input())
 
 
 book = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
