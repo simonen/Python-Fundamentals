3b5a0dca659c50cae00001683313e56fb24817f1
simonen <si.petrov@yahoo.com>
2022-07-10 15:20:59 +0300

    initial commit

--- Personal/01. Decimal To Binary.py
+++ Personal/01. Decimal To Binary.py
@@ -1,9 +1,19 @@
+def bits_count(binary_num_f, num):
+    count = 0
+    for i in binary_num_f:
+        if i == num:
+            count += 1
+
+    return count
+
+
 def binary_num(number_f):
     A = []
     num = int(number_f)
     while num > 0:
         A.append(str(num % 2))
         num = num // 2
+    #Maintain 4bit-sized chunks
     while len(A) % 4 != 0:
         A += "0"
 
@@ -17,6 +27,7 @@ def binary_num(number_f):
 
 
 command = input()
+# bits = input()          # 0/1 count
 
 while command != "stop":
 
@@ -26,3 +37,6 @@ while command != "stop":
         print(binary_num(command))
 
     command = input()
+
+print((binary_num(command)))
+# print(bits_count(binary_num(command), bits))
