7b7f62298c0885ce7a12d44a83aa59c87286a1cc
simonen <si.petrov@yahoo.com>
2022-07-13 02:29:51 +0300

    initial commit

--- Text Processing/Text Processing - Exercise/10. Winning Ticket.py
+++ Text Processing/Text Processing - Exercise/10. Winning Ticket.py
@@ -1,33 +1,41 @@
-tickets = input().split(", ")
-valids = ['@', '#', '$', '^']
+tickets = input().replace(" ", "").split(",")
 
-previous = ""
-side_a = '@'
-side_b = '@'
-special = ''
-count = 0
+valid = ['@', '#', '$', '^']
 
 for ticket in tickets:
+    side_a = ''
+    side_b = ''
+    max_a = ''
+    max_b = ''
+    previous = ''
     if len(ticket) != 20:
-        print("Invalid ticket")
+        print("invalid ticket")
         continue
-    for t in ticket[0:len(ticket) // 2 + 1]:
-        if t in valids and t == previous:
-            special = t
+
+    for t in ticket[0:10]:
+        if t in valid and t == previous:
             side_a += t
-
+            if len(side_a) >= len(max_a):
+                max_a = side_a
+        else:
+            side_a = t
         previous = t
-    for t in ticket[len(ticket) // 2::]:
-        if t in valids and t == previous:
+
+    previous = ''
+    for t in ticket[10::]:
+        if t in valid and t == previous:
             side_b += t
+            if len(side_b) >= len(max_b):
+                max_b = side_b
+        else:
+            side_b = t
         previous = t
-    match = len(side_a)
-    if len(side_a) > len(side_b):
-        match = len(side_b)
 
-    if 6 <= match < 10:
-        print(f"ticket {ticket} - {match}{special}")
-    elif match == 10:
-        print(f"ticket {ticket} - 10{special} Jackpot!")
+    least = min(len(max_a), len(max_b))
+
+    if 6 <= least < 10 and max_a[0] == max_b[0]:
+        print(f'ticket "{ticket}" - {least}{max_a[0]}')
+    elif least == 10:
+        print(f'ticket "{ticket}" - 10{max_a[0]} Jackpot!')
     else:
-        print(f"ticket {ticket} - no match")
+        print(f'ticket "{ticket}" - no match')
