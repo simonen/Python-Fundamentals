fb00f639e568b8e7dca3010741012e683d77e9b9
simonen <si.petrov@yahoo.com>
2022-07-14 00:13:58 +0300

    initial commit

---
+++ Text Processing/Text Processing - More Exercises/04. Morse Code Translator.py
@@ -0,0 +1,24 @@
+text = input().split()
+
+morse = {'A': '.-', 'B': '-...',
+         'C': '-.-.', 'D': '-..', 'E': '.',
+         'F': '..-.', 'G': '--.', 'H': '....',
+         'I': '..', 'J': '.---', 'K': '-.-',
+         'L': '.-..', 'M': '--', 'N': '-.',
+         'O': '---', 'P': '.--.', 'Q': '--.-',
+         'R': '.-.', 'S': '...', 'T': '-',
+         'U': '..-', 'V': '...-', 'W': '.--',
+         'X': '-..-', 'Y': '-.--', 'Z': '--..'}
+
+key_list = list(morse.keys())
+val_list = list(morse.values())
+message = ''
+
+for i in text:
+    if i != "|":
+        position = val_list.index(i)
+        message += key_list[position]
+    else:
+        message += " "
+
+print(message)
