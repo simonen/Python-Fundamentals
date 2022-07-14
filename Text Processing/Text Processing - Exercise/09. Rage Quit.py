7dc8e5162a3393d59bb2bc2af5f7131581a77d90
simonen <si.petrov@yahoo.com>
2022-07-14 23:07:44 +0300

    initial commit

--- Text Processing/Text Processing - Exercise/09. Rage Quit.py
+++ Text Processing/Text Processing - Exercise/09. Rage Quit.py
@@ -1,15 +1,15 @@
 from itertools import groupby
 
-s = input()
+s = input().upper()
 res = [''.join(g) for _, g in groupby(s, str.isdigit)]
 rage = ''
 uniques = ''
 
 for i in range(0, len(res), 2):
     for char in res[i]:
-        if not char.isdigit() and char.lower() not in uniques:
-            uniques += char.lower()
-    rage += res[i].upper() * int(res[i + 1])
+        if not char.isdigit() and char not in uniques:
+            uniques += char
+    rage += res[i] * int(res[i + 1])
 
 print(f"Unique symbols used: {len(uniques)}")
 print(rage)
