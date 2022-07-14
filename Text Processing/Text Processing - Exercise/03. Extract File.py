1f43cbfb182cb194520b91867e145e8f51faa711
simonen <si.petrov@yahoo.com>
2022-07-14 21:05:15 +0300

    initial commit

--- Text Processing/Text Processing - Exercise/03. Extract File.py
+++ Text Processing/Text Processing - Exercise/03. Extract File.py
@@ -1,8 +1,7 @@
 path = input().split("\\")
 
 filename = path[-1].split(".")
-name = filename[0]
-ext = filename[1]
+name, ext = filename
 
 print(f"File name: {name}")
 print(f"File extension: {ext}")
