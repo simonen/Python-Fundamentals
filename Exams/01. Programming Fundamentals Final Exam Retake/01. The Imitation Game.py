b5fda9a10b2151f35e96dffb41195bdb04743920
simonen <si.petrov@yahoo.com>
2022-07-20 19:59:48 +0300

    initial commit

---
+++ Exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.py
@@ -0,0 +1,26 @@
+text = input()
+command = input()
+A = list(text)
+
+while command != "Decode":
+
+    command = command.split("|")
+    action = command[0]
+    if action == "Move":
+        number = int(command[1])
+        if 0 <= number < len(A):
+            A = A[number::] + A[:number]
+    elif action == "Insert":
+        index = int(command[1])
+        value = command[2]
+        A.insert(index, value)
+        A = list("".join(A))
+
+    elif action == "ChangeAll":
+        substring = command[1]
+        replacement = command[2]
+        A = list("".join(A).replace(substring, replacement))
+
+    command = input()
+
+print(f'The decrypted message is: {"".join(A)}')
