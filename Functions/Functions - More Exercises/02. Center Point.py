7a78ddfd234309a4060e026a29ec0e1f35655ead
simonen <si.petrov@yahoo.com>
2022-07-22 17:58:11 +0300

    initial commit

--- Functions/Functions - More Exercises/02. Center Point.py
+++ Functions/Functions - More Exercises/02. Center Point.py
@@ -4,8 +4,7 @@ from math import floor
 def closest_point(a1, b1, a2, b2):
     if abs(a1) <= abs(a2) and abs(b1) <= abs(b2):
         return floor(a1), floor(b1)
-    else:
-        return floor(a2), floor(b2)
+    return floor(a2), floor(b2)
 
 
 x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
