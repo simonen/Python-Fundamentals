413db89717108d40471e04c3ff369d216dd45a45
simonen <si.petrov@yahoo.com>
2022-07-26 18:40:45 +0300

    initial commit

--- Exams/03. Programming Fundamentals Final Exam Retake/02. Mirror Words.py
+++ Exams/03. Programming Fundamentals Final Exam Retake/02. Mirror Words.py
@@ -2,25 +2,21 @@ import re
 
 text = input()
 
-match = re.finditer(r"(?<=(\#|@))([a-zA-Z]{3,})(\1\1)([a-zA-Z]{3,})\1", text)
-valid_pairs = [x.group(2) for x in match]
-match2 = re.finditer(r"(?<=(\#|@))([a-zA-Z]{3,})(\1\1)([a-zA-Z]{3,})\1", text)
-valid_pairs2 = [y.group(4) for y in match2]
+match = re.findall(r"(?<=(\#|@))([a-zA-Z]{3,})(\1\1)([a-zA-Z]{3,})\1", text)
 
-if len(valid_pairs) > 0:
-    print(f"{len(valid_pairs)} word pairs found!")
+if len(match) > 0:
+    print(f"{len(match)} word pairs found!")
 else:
     print("No word pairs found!")
 
 mirrored = []
 
-for i in valid_pairs:
-    for x in valid_pairs2:
-        if x == i[::-1]:
-            mirrored.append(f"{i} <=> {x}")
+for v in match:
+    if v[1] == v[3][::-1]:
+        mirrored.append(f"{v[1]} <=> {v[3]}")
 
 if len(mirrored) > 0:
-    print(f"The mirror words are:")
+    print("The mirror words are:")
     print(", ".join(mirrored))
 else:
     print("No mirror words!")
