2e02d19fb514519b440a16834e19fb0549cd4c94
simonen <si.petrov@yahoo.com>
2022-07-29 21:13:46 +0300

    initial commit

--- Dictionaries/Dictionaries - More Exercises/02.  Judge v2.py
+++ Dictionaries/Dictionaries - More Exercises/02.  Judge v2.py
@@ -12,11 +12,9 @@ while command != "no more time":
         book[username] = {}
     if contest not in courses:
         courses[contest] = 0
-    if contest not in book[username]:
-        book[username][contest] = 0
-        courses[contest] += 1
-    if book[username][contest] < points:
+    if contest not in book[username] or book[username][contest] < points:
         book[username][contest] = points
+        courses[contest] += 1
 
     command = input()
 
@@ -28,7 +26,7 @@ for k, v in book.items():
 for course, participants in courses.items():
     index = 1
     print(f"{course}: {participants} participants")
-    by_course = [x for x in book.items() if course in x[1]]
+    by_course = [x for x in book.items() if course in x[1]] #filter out the participants for the corresponding contest
     for name in sorted(by_course, key=lambda x: (-x[1][course], x[0])):
         print(f"{index}. {name[0]} <::> {name[1][course]}")
         index += 1
