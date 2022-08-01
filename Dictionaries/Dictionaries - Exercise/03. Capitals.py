1043c30aeccf7cfee93637acb10686bea06fe83e
simonen <si.petrov@yahoo.com>
2022-08-01 22:52:56 +0300

    initial commit

--- Dictionaries/Dictionaries - Exercise/03. Capitals.py
+++ Dictionaries/Dictionaries - Exercise/03. Capitals.py
@@ -6,10 +6,10 @@ dict = {countries[i]: cities[i] for i in range(0, len(countries))}
 for k, v in dict.items():
     print(f"{k} -> {v}")
 
-# for i in range(0, len(countries)):
-#     key = countries[i]
-#     value = capitals[i]
-#     dict[key] = value
-
-#print(dict)
-
+# countries = input().split(", ")
+# capitals = input().split(", ")
+#
+# book = dict(zip(countries, capitals))
+#
+# for country, capital in book.items():
+#     print(f"{country} -> {capital}")
