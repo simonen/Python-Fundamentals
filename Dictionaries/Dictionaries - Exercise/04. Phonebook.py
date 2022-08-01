1f6c59535ffda2cce861bd2474bfbe6e380dd5d0
simonen <si.petrov@yahoo.com>
2022-08-01 23:15:34 +0300

    initial commit

--- Dictionaries/Dictionaries - Exercise/04. Phonebook.py
+++ Dictionaries/Dictionaries - Exercise/04. Phonebook.py
@@ -1,20 +1,37 @@
-command = input()
+entries = input()
 book = {}
 
-while True:
-    if command.isnumeric():
-        break
-    phone = command.split("-")
-    name = phone[0]
-    number = phone[1]
+while not entries.isdigit():
+    command = entries.split("-")
+    name = command[0]
+    number = command[1]
     book[name] = number
-    command = input()
 
-for i in range(int(command)):
-    key = input()
-    if key in book.keys():
-        value = book[key]
-        print(f"{key} -> {value}")
+    entries = input()
+
+for entry in range(int(entries)):
+    searched_name = input()
+    if searched_name in book.keys():
+        print(f"{searched_name} -> {book[searched_name]}")
     else:
-        print(f"Contact {key} does not exist.")
+        print(f"Contact {searched_name} does not exist.")
 
+# command = input()
+# book = {}
+#
+# while True:
+#     if command.isnumeric():
+#         break
+#     phone = command.split("-")
+#     name = phone[0]
+#     number = phone[1]
+#     book[name] = number
+#     command = input()
+#
+# for i in range(int(command)):
+#     key = input()
+#     if key in book.keys():
+#         value = book[key]
+#         print(f"{key} -> {value}")
+#     else:
+#         print(f"Contact {key} does not exist.")
