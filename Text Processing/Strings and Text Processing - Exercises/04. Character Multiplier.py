2e65cb4c9d52c9bfde84c81e7b5eae5acc6e2090
simonen <si.petrov@yahoo.com>
2022-08-04 00:00:11 +0300

    initial commit

---
+++ Text Processing/Strings and Text Processing - Exercises/04. Character Multiplier.py
@@ -0,0 +1,17 @@
+text = input().split()
+
+string1, string2 = text
+total = 0
+short = string1
+long = string2
+
+if len(string2) < len(string1):
+    long = string1
+    short = string2
+
+total += sum([ord(x) for x in long[len(short)::]])
+
+for i in range(len(short)):
+    total += ord(short[i]) * ord(long[i])
+
+print(total)
