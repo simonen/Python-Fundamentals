6dffaedccdfa69c9e84812393bbc31de89eaedd1
simonen <si.petrov@yahoo.com>
2022-08-05 01:27:57 +0300

    initial commit

--- Personal/03. Longest Substring.py
+++ Personal/03. Longest Substring.py
@@ -1,22 +1,22 @@
-def longest_substring(string_f, len_f):
+def longest_substring(string_f, subs_len_f):
     store = []
     prev = ''
-    streak_f = ''
+    substring = ''
     for i in range(len(string_f)):
         if string_f[i] == prev:
-            streak_f += string_f[i]
-            if i == len(string_f) - 1 and len(streak_f) > len_f:
-                store.append(streak_f)
+            substring += string_f[i]
+            if i == len(string_f) - 1 and len(substring) >= subs_len_f:
+                store.append(substring)
         else:
-            if len(streak_f) > len_f:
-                store.append(streak_f)
-            streak_f = string_f[i]
+            if len(substring) >= subs_len_f:
+                store.append(substring)
+            substring = string_f[i]
 
         prev = string_f[i]
 
     return store
 
 
-a = "11100huizzzingerrrr011110000ff"
+a = "11100huizzzingerrrr0111100000ff"
 
-print(longest_substring(a, 1))
+print(longest_substring(a, 5))
