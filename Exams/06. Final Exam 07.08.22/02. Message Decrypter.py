16ef1baf3c82af36e456aae917d14aa034e10c79
simonen <si.petrov@yahoo.com>
2022-08-10 00:31:29 +0300

    initial commit

--- Exams/06. Final Exam 07.08.22/02. Message Decrypter.py
+++ Exams/06. Final Exam 07.08.22/02. Message Decrypter.py
@@ -10,3 +10,25 @@ for _ in range(n):
     else:
         a = [x for x in match[0]]
         print(f"{a[1]}: {chr(int(a[2]))}{chr(int(a[3]))}{chr(int(a[4]))}")
+
+#####
+# Sonia Haralambieva
+# import re
+# num = int(input())
+# pattern = r'^(\%|\$)([A-Z][a-z]{2,})\1:\s\[(\d+\]\|\[\d+\]\|\[\d+)\]\|$'
+#
+# for _ in range(num):
+#     current_command = input()
+#     matches = re.match(pattern, current_command)
+#
+#     if not matches:
+#         print("Valid message not found!")
+#     else:
+#         list_of_numbers = re.findall("\d+", matches.group())
+#         ascii_list_of_numbers = [chr(int(num)) for num in list_of_numbers]
+#
+#         current_decrypted_message = "".join(ascii_list_of_numbers)
+#         tag = matches[2]
+#
+#         print(f"{tag}: {current_decrypted_message}")
+
