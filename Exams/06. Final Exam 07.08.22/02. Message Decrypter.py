869b7255206c9e1c150e908d668f090a7dc474e3
simonen <si.petrov@yahoo.com>
2022-08-09 23:51:13 +0300

    initial commit

--- Exams/06. Final Exam 07.08.22/02. Message Decrypter.py
+++ Exams/06. Final Exam 07.08.22/02. Message Decrypter.py
@@ -1,11 +1,10 @@
 import re
 
 n = int(input())
-for _ in range(n):
 
+for _ in range(n):
     string = input()
     match = re.findall(r"^(\$|\%)([A-Z][a-z]{2,})\1: \[([0-9]+)\]\|\[([0-9]+)\]\|\[([0-9]+)\]\|($|\s)", string)
-    # print(match)
     if len(match) == 0:
         print("Valid message not found!")
     else:
