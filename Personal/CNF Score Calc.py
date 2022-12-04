fc5872831fc425a8a15e2ceca554e21d52c898c1
simonen <si.petrov@yahoo.com>
2022-12-04 13:06:03 +0200

    initial commit

--- Personal/CNF Score Calc.py
+++ Personal/CNF Score Calc.py
@@ -3,13 +3,14 @@ def score_calc(pract_f, theory_f):
         max_points = 187 * 0.65 + 23 * 0.35
         coeff = float(f"{(max_points / 6):.2f}")
         scored_pts = (pract_f * 0.65 + theory_f * 0.35) / coeff
-
-        print(f"Your score is: {scored_pts:.2f}")
+        print()
+        print(f"Your score is: {scored_pts:.2f}!")
     else:
         print("Invalid points")
 
 
 print("### Computer Networking Fundamentals Score Calculator ### by don_simone")
+print()
 
 while True:
     score_calc(int(input('Enter practice test points: ')), int(input('Enter theory test points: ')))
