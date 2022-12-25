4bdbd8f548266886d42171a7a920dd3bef220443
simonen <si.petrov@yahoo.com>
2022-12-26 00:52:29 +0200

    initial commit

--- Personal/base64_encoder.py
+++ Personal/base64_encoder.py
@@ -1,33 +1,33 @@
 upper = [chr(x) for x in range(65, 91)]
 lower = [chr(x) for x in range(97, 123)]
-digits_upper = [x for x in range(0, 26)]
-digits_lower = [x for x in range(26, 52)]
+index_upper = [x for x in range(0, 26)]
+index_lower = [x for x in range(26, 52)]
 digits = [x for x in range(0, 10)]
-digits_index = [x for x in range(52, 62)]
+index_digits = [x for x in range(52, 62)]
 
-upper_dict = dict(zip(digits_upper, upper))
-lower_dict = dict(zip(digits_lower, lower))
-digits_dict = dict(zip(digits_index, digits))
+b64_table = dict(zip(index_upper, upper))
+lower_dict = dict(zip(index_lower, lower))
+digits_dict = dict(zip(index_digits, digits))
 chars = {62: '+', 63: "/"}
 
-upper_dict.update(lower_dict)
-upper_dict.update(digits_dict)
-upper_dict.update(chars)
+b64_table.update(lower_dict)
+b64_table.update(digits_dict)
+b64_table.update(chars)
 
 
 def b64_encode(input_f, b64_table_f, splitter_f):
-    word_binary = [ord(x) for x in input_f]
-    octets = [f'{x:08b}' for x in word_binary]
-    binary_conc = "".join(octets)
-    if len(binary_conc) % 6 != 0:
-        binary_conc += "0" * (6 - (len(binary_conc) % 6))
+    ascii_dec = [ord(x) for x in input_f]
+    octets = [f'{x:08b}' for x in ascii_dec]
+    octets_concatenated = "".join(octets)
+    if len(octets_concatenated) % 6 != 0:
+        octets_concatenated += "0" * (6 - (len(octets_concatenated) % 6))
 
-    A = splitter_f(binary_conc)
-    sextets = [int(x, 2) for x in A]
-    enc = [str(b64_table_f[x]) for x in sextets]
-    if len(enc) < 4:
-        enc += list('=' * (4 - (len(enc))))
-    return enc
+    sextets = splitter_f(octets_concatenated)
+    b64_dec = [int(x, 2) for x in sextets]
+    b64_encoded = [str(b64_table_f[x]) for x in b64_dec]
+    if len(b64_encoded) < 4:
+        b64_encoded += list('=' * (4 - (len(b64_encoded))))
+    return b64_encoded
 
 
 def str_splitter(string_f):
@@ -38,18 +38,19 @@ def str_splitter(string_f):
     return grouped
 
 
-contents = []
 while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if len(contents) > 0:
-        contents.append('\n' + line)
-    else:
-        contents.append(line)
-print(contents)
+    contents = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        if len(contents) > 0:
+            contents.append('\n' + line)
+        else:
+            contents.append(line)
+    print(contents)
 
-contentz = "".join(contents)
-encoded = b64_encode(contentz, upper_dict, str_splitter)
-print("".join(encoded))
+    contentz = "".join(contents)
+    encoded = b64_encode(contentz, b64_table, str_splitter)
+    print("".join(encoded))
