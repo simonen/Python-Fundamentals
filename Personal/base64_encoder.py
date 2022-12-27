5ad3a01ef42a480b6a6596b77aad95fefffc4b43
simonen <si.petrov@yahoo.com>
2022-12-27 21:40:35 +0200

    initial commit

--- Personal/base64_encoder.py
+++ Personal/base64_encoder.py
@@ -14,19 +14,29 @@ b64_table.update(lower_dict)
 b64_table.update(digits_dict)
 b64_table.update(chars)
 
+# print('base64 table: ', b64_table)
+
 
 def b64_encode(input_f, b64_table_f, splitter_f):
     ascii_dec = [ord(x) for x in input_f]
+    print('ascii decimals: ', ascii_dec)
     octets = [f'{x:08b}' for x in ascii_dec]
+    print('binary octets: ', octets)
     octets_concatenated = "".join(octets)
+    print('concatenated binary: ', octets_concatenated)
     if len(octets_concatenated) % 6 != 0:
         octets_concatenated += "0" * (6 - (len(octets_concatenated) % 6))
 
     sextets = splitter_f(octets_concatenated)
+    print('binary sextets: ', sextets)
     b64_dec = [int(x, 2) for x in sextets]
+    print('base64 decimals: ', b64_dec)
     b64_encoded = [str(b64_table_f[x]) for x in b64_dec]
-    if len(b64_encoded) < 4:
-        b64_encoded += list('=' * (4 - (len(b64_encoded))))
+    padding = ''
+    if len(b64_encoded) % 4 != 0:
+        padding = '=' * (4 - len(b64_encoded) % 4)
+        b64_encoded += padding
+    print('padding: ', len(padding))
     return b64_encoded
 
 
@@ -43,7 +53,7 @@ def multiline_text():
     while True:
         try:
             line = input()
-            if line == "encode":
+            if line == "enc":
                 break
         except EOFError:
             break
@@ -58,5 +68,6 @@ def multiline_text():
 while True:
     text = multiline_text()
     encoded = b64_encode(text, b64_table, str_splitter)
-    print("".join(encoded))
+    print('string length: ', len(text))
+    print('encoded string: ', "".join(encoded))
 
