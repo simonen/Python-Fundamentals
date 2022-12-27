1514f896c6d74b8e1c5104eb6cdc1cda045fcbc1
simonen <si.petrov@yahoo.com>
2022-12-28 01:16:21 +0200

    initial commit

--- Personal/base64_encoder.py
+++ Personal/base64_encoder.py
@@ -1,8 +1,9 @@
+
 upper = [chr(x) for x in range(65, 91)]
 lower = [chr(x) for x in range(97, 123)]
 index_upper = [x for x in range(0, 26)]
 index_lower = [x for x in range(26, 52)]
-digits = [x for x in range(0, 10)]
+digits = [str(x) for x in range(0, 10)]
 index_digits = [x for x in range(52, 62)]
 
 b64_table = dict(zip(index_upper, upper))
@@ -40,10 +41,25 @@ def b64_encode(input_f, b64_table_f, splitter_f):
     return b64_encoded
 
 
+def b64_decode(b64_keys_f, splitter_f):
+    print('string length: ', len(b64_keys_f))
+    sextets = [f"{x:06b}" for x in b64_keys_f]
+    print('sextets: ', sextets)
+    sextets_conc = "".join(sextets)
+    print('sextets conc: ', sextets_conc)
+    octets = splitter_f(sextets_conc)
+    print('octets: ', octets)
+    ascii_decimal = [int(x, 2) for x in octets]
+    print('ascii decimals: ', ascii_decimal)
+    decoded_chars = [chr(x) for x in ascii_decimal]
+
+    return "".join(decoded_chars)
+
+
 def str_splitter(string_f):
     grouped = []
-    for i in range(0, len(string_f), 6):
-        chunk = string_f[i:i + 6]
+    for i in range(0, len(string_f), 8):
+        chunk = string_f[i:i + 8]
         grouped.append(chunk)
     return grouped
 
@@ -65,9 +81,25 @@ def multiline_text():
     return "".join(contents)
 
 
-while True:
-    text = multiline_text()
-    encoded = b64_encode(text, b64_table, str_splitter)
-    print('string length: ', len(text))
-    print('encoded string: ', "".join(encoded))
+def keys(b64_table_f, b64_string_f):
+    b64_keys = []
+    for char in b64_string_f:
+        if str(char) in b64_table_f.values():
+            b64_keys.append((list(b64_table_f.keys())[list(b64_table_f.values()).index(char)]))
+    return b64_keys
 
+# while True:
+#     text = multiline_text()
+#     encoded = b64_encode(text, b64_table, str_splitter)
+#     print('string length: ', len(text))
+#     print('encoded string: ', "".join(encoded))
+#
+
+msg = input()
+
+deco = b64_decode(keys(b64_table, msg), str_splitter)
+# chur = deco.decode('ascii')
+
+print(deco)
+# print(chur)
+# print(keys(b64_table, input()))
