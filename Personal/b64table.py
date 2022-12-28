27f8d326224814794eea999a2dd2f46115561eab
simonen <si.petrov@yahoo.com>
2022-12-29 00:07:50 +0200

    initial commit

--- Personal/b64table.py
+++ Personal/b64table.py
@@ -8,9 +8,9 @@ base64_message = base64_bytes.decode('utf8')
 print(base64_message)
 
 # base64_message = input()
-# base64_bytes = base64_message.encode('ascii')
+# base64_bytes = base64_message.encode('utf8')
 # print(base64_bytes)
 # message_bytes = base64.b64decode(base64_bytes)
-# message = message_bytes.decode('ascii')
+# message = message_bytes.decode('utf8')
 #
 # print(message)
