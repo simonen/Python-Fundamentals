7b91b6b018e76e01d9d1cdb456c7e1e40e6e775d
simonen <si.petrov@yahoo.com>
2023-01-15 00:03:46 +0200

    initial commit

--- Personal/Caesar Cipher.py
+++ Personal/Caesar Cipher.py
@@ -28,17 +28,14 @@ upper = [chr(x) for x in range(65, 91)]
 command = input("<encrypt/decrypt> <shift> ")
 while True:
     command = command.split()
+    shift = int(command[1])
+    cipher_lower = [lower[(lower.index(x) + shift) % len(lower)] for x in lower]
+    cipher_upper = [upper[(upper.index(x) + shift) % len(upper)] for x in upper]
     if command[0] == "encrypt":
         string = input("Enter a string to encrypt: ")
-        shift = int(command[1])
-        cipher_lower = [lower[(lower.index(x) + shift) % len(lower)] for x in lower]
-        cipher_upper = [upper[(upper.index(x) + shift) % len(upper)] for x in upper]
         print("Encrypted text:", caesar_encrypt(lower, upper, cipher_lower, cipher_upper, string))
     elif command[0] == "decrypt":
         string = input("Enter a string to decrypt: ")
-        shift = int(command[1])
-        cipher_lower = [lower[(lower.index(x) + shift) % len(lower)] for x in lower]
-        cipher_upper = [upper[(upper.index(x) + shift) % len(upper)] for x in upper]
         print("Deciphered text:", caesar_decrypt(lower, upper, cipher_lower, cipher_upper, string))
 
-    command = input("encrypt or decrypt? ")
+    command = input("<encrypt/decrypt> <shift> ")
