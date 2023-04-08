4d42498bd9f03ae4ef6ec776d7cf28cb2e59ada2
simonen <si.petrov@yahoo.com>
2023-04-08 22:59:25 +0300

    initial commit

--- Data Types and Variables/Data Types and Variables - Exercise/08. Party Profit.py
+++ Data Types and Variables/Data Types and Variables - Exercise/08. Party Profit.py
@@ -1,23 +1,23 @@
 group_size = int(input())
 days = int(input())
+
+day = 0
 coins = 0
 
 for day in range(1, days + 1):
-    motivation = 0
+    is_motivational_day = False
     if day % 10 == 0:
         group_size -= 2
     if day % 15 == 0:
         group_size += 5
     if day % 3 == 0:
         coins -= 3 * group_size
-        motivation = 1
+        is_motivational_day = True
     if day % 5 == 0:
         coins += 20 * group_size
-        if motivation == 1:
+        if is_motivational_day:
             coins -= 2 * group_size
-    coins += 50
-    coins -= 2 * group_size
 
-coin_per_com = coins // group_size
+    coins += 50 - (2 * group_size)
 
-print(f"{group_size} companions received {coin_per_com} coins each.")
+print(f"{group_size} companions received {coins // group_size} coins each.")
