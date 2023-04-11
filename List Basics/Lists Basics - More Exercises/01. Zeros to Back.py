214740b1d74a9029ba14e77a96fa132ee2d4d400
simonen <si.petrov@yahoo.com>
2023-04-11 23:06:09 +0300

    initial commit

--- List Basics/Lists Basics - More Exercises/01. Zeros to Back.py
+++ List Basics/Lists Basics - More Exercises/01. Zeros to Back.py
@@ -1,13 +1,13 @@
-integers = input()
+digits = list(map(int, input().split(", ")))
+zeroz = [0 for x in range(0, digits.count(0))]
+non_zeroes = [x for x in digits if x != 0]
+non_zeroes.extend(zeroz)
+print(non_zeroes)
 
-A = integers.split(", ")
-B = []
-C = []
+# Variant 2
+for i in digits:
+    if i == 0:
+        digits.remove(i)
+        digits.append(i)
 
-for num in A:
-    if int(num) != 0:
-        B.append(int(num))
-    else:
-        C.append(int(num))
-
-print(B + C)
+print(digits)
