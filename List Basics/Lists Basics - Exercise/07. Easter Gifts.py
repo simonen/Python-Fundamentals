73027f0ff71a6cdd147f5ab89933dc47a3abcf17
simonen <si.petrov@yahoo.com>
2023-04-11 23:49:16 +0300

    initial commit

--- List Basics/Lists Basics - Exercise/07. Easter Gifts.py
+++ List Basics/Lists Basics - Exercise/07. Easter Gifts.py
@@ -1,20 +1,33 @@
-gifts = input().split()
+gifts = input()
 command = input()
 
-while command != "No Money":
-    command = command.split()
-    action = command[0]
-    gift = command[1]
-    if action == 'OutOfStock':
-        gifts = ["None" if x == gift else x for x in gifts]
-    elif action == "Required":
-        index = int(command[2])
-        if 0 <= index < len(gifts):
-            gifts[index] = gift
-    elif action == "JustInCase":
-        gifts[-1] = gift
+gift_list = gifts.split(" ")
 
+while command != "No Money":
+
+    com = command.split()
+    gift = com[1]
+    commanda = com[0]
+    if len(com) == 3:
+        given_index = int(com[2])
+        length = len(gift_list)
+        if 0 <= given_index < length:
+            gift_list.pop(given_index)
+            gift_list.insert(given_index, gift)
+    if commanda == "OutOfStock":
+        for _ in range(len(gift_list)):
+            if gift in gift_list:
+                g_index = gift_list.index(gift)
+                gift_list.pop(g_index)
+                gift_list.insert(g_index, "None")
+    elif commanda == "JustInCase":
+        gift_list.pop()
+        gift_list.append(gift)
     command = input()
 
-gifts = [x for x in gifts if x != "None"]
-print(" ".join(gifts))
+for _ in range(len(gift_list)):
+    if "None" in gift_list:
+        g_index = gift_list.index("None")
+        gift_list.pop(g_index)
+
+print(" ".join(gift_list))
